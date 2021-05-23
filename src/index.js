d797feaa20d8af50a320e6d150a67b5d2de7dcee
yCroma <kuroyuki0728@gmail.com>
2021-05-23 22:27:07 +0900

    ADD: index file

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import WikiHeader from "Components/wiki-header/wiki-header"
+import WikiTitle from "Components/wiki-title/wiki-title"
+import WikiContent from "Components/wiki-content/wiki-content"
+import WikiTree from "Components/wiki-tree/wiki-tree"
+import WikiArticle from "Components/wiki-article/wiki-article"
+import WikiFooter from "Components/wiki-footer/wiki-footer"
+import WikiContact from "Components/wiki-contact/wiki-contact"
+
+import spa from "GlobalAffects/spa"
+console.log("test")
+
