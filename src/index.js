f1061980ec1556b34f4baba863108c643d2dabda
yCroma <kuroyuki0728@gmail.com>
2021-05-26 16:10:17 +0900

    UPDATE: import Components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import WikiHeader from "Components/wiki-header/wiki-header"
-import WikiTitle from "Components/wiki-title/wiki-title"
-import WikiContent from "Components/wiki-content/wiki-content"
-import WikiTree from "Components/wiki-tree/wiki-tree"
-import WikiArticle from "Components/wiki-article/wiki-article"
-import WikiFooter from "Components/wiki-footer/wiki-footer"
-import WikiContact from "Components/wiki-contact/wiki-contact"
+import WikiHeader from "Components/modules/wiki-header"
+import WikiTitle from "Components/molds/wiki-title"
+
+import WikiContent from "Components/modules/wiki-content"
+import WikiTree from "Components/parts/wiki-tree"
+import WikiArticle from "Components/parts/wiki-article"
+
+import WikiFooter from "Components/modules/wiki-footer"
+import WikiContact from "Components/molds/wiki-contact"
 
 import spa from "GlobalAffects/spa"
-console.log("test")
 
