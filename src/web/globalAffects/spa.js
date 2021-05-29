c742022cb77d10c3557c680c896db46baca03821
yCroma <kuroyuki0728@gmail.com>
2021-05-29 14:25:23 +0900

    UPDATE: shadowDOMに対応

--- src/web/globalAffects/spa.js
+++ src/web/globalAffects/spa.js
@@ -4,20 +4,35 @@ import updatepathname from "Events/updatepathname"
  * よって、遷移はブロックして、パスネームだけ変更してレンダリング
  */
 
+/*
+ * 全ての child は shadowRoot の下にコンポーネントを所持している
+ * childの子要素の shadowRoot を探す
+ */
+
 // 再帰して全てのshadow dom内にある anchor link にイベントを追加
 function addPreventDefault2anker(NodeList) {
 	Array.from(NodeList, child => {
-		if(child.children.length) {
-			addPreventDefault2anker(child.children)
-		}
+		// child の shadowRoot が a タグを持っていればイベント追加
 		try {
 			child.shadowRoot.querySelectorAll("a").forEach( a => {
 				a.addEventListener("click", event => {
 					event.preventDefault();
 					window.history.pushState(null, "", a.href)
-					document.querySelector("wiki-article").dispatchEvent(updatepathname)
+					// wiki-content 下の wiki-articleに対してイベントディスパッチ
+					document.querySelector("wiki-content")
+						.shadowRoot.querySelector("wiki-article")
+							.dispatchEvent(updatepathname)
 				})
 			})
+		} catch {
+			// shadow root がない場合のエラーをキャッチ
+		}
+
+		// child の shadowRoot が子要素を持っていれば再帰
+		try {
+			if(child.shadowRoot.children.length) {
+				addPreventDefault2anker(child.shadowRoot.children)
+			}
 		} catch {
 			// shadow root がない場合のエラーをキャッチ
 		}
@@ -29,5 +44,7 @@ addPreventDefault2anker(document.querySelector("body").children)
 
 window.addEventListener("popstate", () => {
 	// updatepathname event で更新
-	document.querySelector("wiki-article").dispatchEvent(updatepathname)
+	document.querySelector("wiki-content")
+		.shadowRoot.querySelector("wiki-article")
+		.dispatchEvent(updatepathname)
 })
